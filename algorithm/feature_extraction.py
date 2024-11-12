332acb67903da2d9b4133c269e87bd4b43ccdb01
Francesc Pei Purroy <frpp@kth.se>
2024-11-12 16:57:08 +0100

    directory work

--- algorithm/feature_extraction.py
+++ algorithm/feature_extraction.py
@@ -61,12 +61,23 @@ def _read_slice_of_file(file_path, start, end):
     return slice_lines
 
 
-def full_read(directory, scene):
+def full_read(directory, scene, save):
     """
     Reads and processes full dataset 0/1/2
 
-    >> directory: path to directory with data of form DatasetX
-    >> scene = 1, 2 or 3, specifies a scene from dataset X
+    >> directory: path to directory of form DatasetX
+    >> scene = 1, 2 or 3: specifies a scene from dataset X
+    >> save: bool
+
+    Return values:
+    H: channel data
+    anch_pos: anchor ID and its coordinates
+    bs_pos: coordinate of the base station
+    tol_samp_num: total number of channel data points
+    anch_samp_num: total number of anchor points
+    port_num: number of SRS Ports (number of antennas for the UE)
+    ant_num: number of antennas for the base station
+    sc_num: number of subcarriers
     """
 
     files = os.listdir(directory)
@@ -81,58 +92,57 @@ def full_read(directory, scene):
     else:
         demo = False
 
-    cfg_files = []
-    anchor_files = []
-    channel_files = []
-    ground_files = []
+    cfg_file = ""
+    anchor_file = ""
+    channel_file = ""
+    ground_file = ""
     for f in files:
-        if "Cfg" in f:
-            cfg_files.append(f)
+        if "Cfg" in f[:]:
+            cfg_file = f
         if "Pos" in f:
-            anchor_files.append(f)
+            anchor_file = f
         if "InputData" in f:
-            channel_files.append(f)
+            channel_file = f
         if demo:
             if "Ground" in f:
-                ground_files.append(f)
+                ground_file = f
 
-    print('Loading cfg files')
-    for f in cfg_files:
-        cfg_file_path = directory + '/' + f
-        bs_pos, tol_samp_num, anch_samp_num, port_num, ant_num, sc_num = read_cfg_file(cfg_file_path)
+    print('Loading cfg file')
+    cfg_file_path = directory + '/' + cfg_file
+    bs_pos, tol_samp_num, anch_samp_num, port_num, ant_num, sc_num = read_cfg_file(cfg_file_path)
 
-    print('Loading anchor files')
-    for f in anchor_files:
-        anch_file_path = directory + '/' + f
-        anch_pos = read_anch_file(anch_file_path, anch_samp_num)
+    print('Loading anchor file')
+    anch_file_path = directory + '/' + anchor_file
+    anch_pos = read_anch_file(anch_file_path, anch_samp_num)
 
-    print('Loading channel data files')
-    for f in channel_files:
-        slice_samp_num = 1000  # number of samples in each slice
-        slice_num = int(tol_samp_num / slice_samp_num)  # total number of slices
-        csi_file_path = directory + '/' + f
-        H = []
+    print('Loading channel data file')
+    slice_samp_num = 1000  # number of samples in each slice
+    slice_num = int(tol_samp_num / slice_samp_num)  # total number of slices
+    csi_file_path = directory + '/' + channel_file
+    H = []
 
-        for slice_idx in range(slice_num):  # range(slice_num): # Reads a channel per loop
+    for slice_idx in range(slice_num):  # range(slice_num): # Reads a channel per loop
 
-            print('Loading input CSI data of slice ' + str(slice_idx) + "of " + str(slice_num))
-            slice_lines = _read_slice_of_file(csi_file_path, slice_idx * slice_samp_num, (slice_idx + 1) * slice_samp_num)
-            Htmp = np.loadtxt(slice_lines)
-            Htmp = np.reshape(Htmp, (slice_samp_num, 2, sc_num, ant_num, port_num))
-            Htmp = Htmp[:, 0, :, :, :] + 1j * Htmp[:, 1, :, :, :]
-            Htmp = np.transpose(Htmp, (0, 3, 2, 1))
-            if np.size(H) == 0:
-                H = Htmp
-            else:
-                H = np.concatenate((H, Htmp), axis=0)
+        print('Loading input CSI data of slice ' + str(slice_idx) + "of " + str(slice_num))
+        slice_lines = _read_slice_of_file(csi_file_path, slice_idx * slice_samp_num, (slice_idx + 1) * slice_samp_num)
+        Htmp = np.loadtxt(slice_lines)
+        Htmp = np.reshape(Htmp, (slice_samp_num, 2, sc_num, ant_num, port_num))
+        Htmp = Htmp[:, 0, :, :, :] + 1j * Htmp[:, 1, :, :, :]
+        Htmp = np.transpose(Htmp, (0, 3, 2, 1))
+        if np.size(H) == 0:
+            H = Htmp
+        else:
+            H = np.concatenate((H, Htmp), axis=0)
 
-        H = H.astype(np.complex64)  # trunc to complex64 to reduce storage
+    H = H.astype(np.complex64)  # trunc to complex64 to reduce storage
 
+    if save:
         csi_file = directory + '/' + '..' + '/' + 'npy_files' + '/' + f[:-4] + '.npy'
         np.save(csi_file, H)  # save file as binary npy
         # H = np.load(csi_file) # if saved in npy, you can load npy file instead of txt
 
+    return H, anch_pos, bs_pos, tol_samp_num, anch_samp_num, port_num, ant_num, sc_num
+
 
 if __name__ == "__main__":
     pass
-
